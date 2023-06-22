94a3033938d385f41e5c9e0165b92029facb7768
Ace Taffy <xucgphi@gmail.com>
2023-06-22 14:20:46 +0900

    specific update on plotting using pyvista

--- cpes/points3d.py
+++ cpes/points3d.py
@@ -97,17 +97,61 @@ class Points3D:
         return np.allclose(self.data[center_point_cloud(self.data)], np.zeros(3))
         #return np.linalg.norm(self.data[center_point_cloud(self.data)])<1e-5
 
-    def plot(self):# plot_plotly
+    def plot(self):
+        """
+        Plot the point cloud using plotly
+        """
         if any('SPYDER' in name for name in os.environ):
-            print("Detected Spyder. Using pyvista insteat of plotly.")
+            print("Spyder detected. Switching to PyVista for plotting.")
             self.plot_pyvista()
         else:
-            pass
-        df=self.df
-        df['c']=[ord(char) for char in self.df['type']]
-        fig = px.scatter_3d(df, x='x', y='y', z='z',
-                  color='c')
-        fig.show()
+            df = self.df
+            df['c'] = [ord(char) for char in self.df['type']]
+            fig = px.scatter_3d(df, x='x', y='y', z='z', color='c', hover_name=df.index)
+            fig.show()
+
+
+    def plot_pyvista_h(self, backend='pythreejs', highlight:list=None):
+        """
+        Plot the point cloud using pyvista
+        points are added to the plotter color by color
+        otherwise the displayed colors will not all be correct
+        """
+        #TODO add legend atom type-color
+        plotter=pyvista.Plotter()
+        sphere = pyvista.Sphere(radius=0.2, phi_resolution=30, theta_resolution=30)
+        self._add_color()
+        
+        for t in set(self.df.type):
+            typedf_highlight=self.df.loc[highlight][self.df.type==t]
+            typedf_dimmed=self.df.loc[~self.df.index.isin(highlight)][self.df.type==t]
+            # import pdb; pdb.set_trace()
+            def add_mesh(typedf,opacity=1):
+                xyz=typedf.loc[:,['x','y','z']].to_numpy(dtype=float)
+                color=ListedColormap(typedf['color'].to_numpy())
+                pdata = pyvista.PolyData(xyz)
+                pdata['orig_sphere'] = np.arange(len(xyz))
+                pc = pdata.glyph(scale=False, geom=sphere)
+                plotter.add_mesh(pc, cmap=color, show_scalar_bar=False, opacity=opacity)
+            # add mesh if not empty
+            if len(typedf_highlight)>0:
+                add_mesh(typedf_highlight,opacity=1)
+            if len(typedf_dimmed)>0:
+                add_mesh(typedf_dimmed,opacity=0.412)
+
+        # Draw lines between specified points
+        pairs = [(0, 1), (0, 2), (0, 3), (1, 2), (1, 3), (2, 3), (1, 4), (2, 4), (3, 4)]
+        for pair in pairs:
+            point1 = self.df.loc[highlight[pair[0]], ['x', 'y', 'z']].values
+            point2 = self.df.loc[highlight[pair[1]], ['x', 'y', 'z']].values
+            # import pdb; pdb.set_trace()
+            line = pyvista.Line(list(point1), list(point2))
+            plotter.add_mesh(line, color='green', line_width=3)  # Adjust color and line_width as needed
+
+        plotter.set_background("royalblue", top="aliceblue")
+        plotter.show()
+
+
 
     def plot_pyvista(self,backend='pythreejs'):
         """
