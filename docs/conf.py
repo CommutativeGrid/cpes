94dd2651d3c6eeb291e26626cf50140cec10b61e
Ace Taffy <xucgphi@gmail.com>
2023-11-16 16:33:13 +0900

    change conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -16,6 +16,7 @@ html_theme = 'sphinx_rtd_theme'
 import os
 import sys
 sys.path.insert(0, os.path.abspath('..'))
+import cpes
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
